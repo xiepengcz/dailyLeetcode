ee14bf758e0090b22e7aa1e75143e497df12015e
谢芃 <949026370@qq.com>
2022-05-25 23:05:11 +0800

    update

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -11,29 +11,25 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
+ * 使用递归遍历两个链表
  */
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {
-  let l1 = list1
-  let l2 = list2
-  let l3 = { val: 0, next: null }
-  while (l1 || l2) {
-    if (l1.val > l2.val) {
-      l3.val = l2.val
-      l3.next = l1
-    } else {
-      l3.val = l1.val
-      l3.next = l2
-    }
-    l1 = l1.next
-    l2 = l2.next
+var mergeTwoLists = function (l1, l2) {
+  if (l1 === null) {
+    return l2
+  } else if (l2 === null) {
+    return l1
+  } else if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
   }
-  if (l1.next == null) { }
-
 };
 // @lc code=end
 
