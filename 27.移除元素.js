0e0d3e89b44068edb2f209a7a7417bb91902cffe
谢芃 <949026370@qq.com>
2022-06-17 10:21:35 +0800

    update

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -2,6 +2,9 @@
  * @lc app=leetcode.cn id=27 lang=javascript
  *
  * [27] 移除元素
+ * 快慢指针来进行处理 因为需要在原数组下修改 当 fast 指针下的值与 val 不相等，则对应数组的low 指针下的值与其相等，并且 low++，fast++
+ * 当 fast 指针与 val相等，fast++
+ * 最后返回 low
  */
 
 // @lc code=start
@@ -11,15 +14,15 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  let fast = 0
-  let flag = true
-  nums.sort()
-  while (flag) {
-    if (nums[fast] === val) {
-      nums
+  const n = nums.length;
+  let low = 0;
+  for (let fast = 0; fast < n; fast++) {
+    if (nums[fast] !== val) {
+      nums[low] = nums[fast];
+      low++;
     }
   }
-  return nums.length
+  return low;
 };
 // @lc code=end
 
